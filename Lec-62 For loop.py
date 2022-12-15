59861693fe26cfe2b7166f9a5be37421595a158f
Anuj Dadwal <114793954+ANUJ3DADWAL@users.noreply.github.com>
2022-12-15 21:38:00 +0530

    Add files via upload

---
+++ Lec-62 For loop.py
@@ -0,0 +1,16 @@
+# Name="Anuj Dadwal"
+# for i in Name:
+#     print(i)
+
+# for i in range(10):
+#     print("Anuj Dadwal")
+
+# for i in range(0,10):
+#     print("Hello World!")
+
+total=0
+for i in range(1,11):
+    total+=i
+print(total)
+
+
